d55cbafc3e6c1c6ae7599cc86a3bcedf550d2c87
oreotrollturbo <oreotrollturbo@gmail.com>
2024-08-20 03:21:08 +0300

    first commit

---
+++ src/main/java/org/oreo/rallycap/Rally_cap.kt
@@ -0,0 +1,13 @@
+package org.oreo.rallycap
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Rally_cap : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
